be4d9e288ea844cc1f345443694051bf5340cba6
lamarrhaynes <lamarrhaynes@gmail.com>
2019-05-20 19:27:24 -0500

    apply linting to index.js

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 /* eslint-disable no-loop-func */
 import Navigation from './components/Navigation';
-import Content from "./components/Content";
-import Footer from "./components/Footer";
+import Content from './components/Content';
+import Footer from './components/Footer';
 
 const states = {
-  'home': {
-    'title': "LaMarrAHaynes Savvy Code Portfolio"
-  },
-
-
-
-
-'contact': {
-  'title': "welcome to contact page"
-},
-'about': {
-  'title': "welcome to about page"
-}
+    'home': {
+        'title': 'LaMarrAHaynes Savvy Code Portfolio'
+    },
+    'contact': {
+        'title': 'welcome to contact page'
+    },
+    'about': {
+        'title': 'welcome to about page'
+    }
 };
 
 const root = document.querySelector('#root');
 
-function render(state) {
-  root.innerHTML = `
+function render(state){
+    root.innerHTML = `
     ${Navigation(state)}
     ${Content(state)}
     ${Footer(state)}
@@ -31,13 +27,13 @@ function render(state) {
 
 render(states.home);
 
-const links = document.querySelectorAll("nav a");
+const links = document.querySelectorAll('nav a');
 
 links.forEach((link) => {
-  link.addEventListener('click',(e) => {
-    e.preventDefault();
-    render(states, e.target.textContent.toLowerCase());
-  });
+    link.addEventListener('click',(e) => {
+        e.preventDefault();
+        render(states, e.target.textContent.toLowerCase());
+    });
 });
 
 // document.querySelectorAll('na a').addEventListener('click',(event) => {
